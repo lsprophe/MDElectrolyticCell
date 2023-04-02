78b58977766d877c0d27b3a1e8810c2aad2b4be8
Lauren Prophet <laur.prophet@gmail.com>
2023-04-02 13:45:05 -0400

    adding updated MD code that cleanly allows multiple particles of different masses - NOTE untested

---
+++ md.py
@@ -0,0 +1,79 @@
+import numpy as np
+
+# globals
+KBT = 1
+
+
+class Particle:
+    def __init__(self, k, mass, q_init, v_init, force_fun: function):
+        self.k = k
+        self.mass = mass
+        self.q = q_init
+        self.v = v_init
+        self.force_fun = force_fun
+
+        self.q_arr = []
+        self.force_arr = []
+        self.v_arr = []
+
+        # NVT constants
+        self.c1 = None
+        self.c2 = None
+
+    @property
+    def force(self):
+        return self.force_fun(self.k, self.q)
+
+    def update_v(self, dt):
+        self.v = self.v+(self.force/self.mass)*dt
+        
+    def update_q(self, dt):
+        self.q = self.q + dt*self.v
+    
+    def update_histories(self):
+        self.q_arr.append(self.q)
+        self.v_arr.append(self.v)
+        self.force_arr.append(self.force_arr)
+
+def NVT_constants(gamma, dt, mass):
+    c1 = np.exp(-gamma*dt)
+    c2 = np.sqrt((1-np.exp(-2*gamma*dt))*KBT/mass)
+    return c1, c2
+
+def verlet_integrator(particles: list, n_steps, dt, force: function, system='NVE', gamma=None, skip=0):
+    t_arr = [i*dt for i in range(n_steps+1)]
+    n_par = len(particles)
+    if system == 'NVT':
+        # set NVT constants for each particle
+        for p in particles:
+            p.c1, p.c2 = NVT_constants(gamma, dt, p.mass)
+
+        # NOTE: not sure if this should be random uniform or random normal???
+        # NOTE: I calculate all the random numbers needed ahead of time for speed
+        rands = np.random.rand(n_par, n_steps)
+
+    # iterate over time steps (keep track of step in case steps are skipped)
+    i = 0
+    while i < n_steps:
+        # iterate over particles in particle list
+        for pi, p in enumerate(particles):
+            p.update_v(dt/2.)
+            if system == 'NVE':
+                p.update_q(dt)
+            elif system == 'NVT':
+                p.update_q(dt/2.)
+
+                # apply random motion
+                rand = rands[pi][i]
+                p.v = p.c1*p.v + rand*p.c2
+
+                p.update_q(dt/2.)
+
+            p.update_v(dt/2.)
+
+            if i % (skip + 1) == 0:
+                # update arrays if this is not a step to skip
+                p.update_histories()
+            i += 1
+
+    return t_arr
