fixing units (everything in nm now)

MD/forces.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 # globals
-K_E = 8.988e9  #coulomb constant
+K_E = 8.988e27  #coulomb constant (N*nm^2/C^2)
 
 # simple two atom potential functions
 def LJF_attractive(l1, l2, eps, sigma):
@@ -17,7 +17,7 @@ def LJF_attractive(l1, l2, eps, sigma):
     r_ij = np.sqrt(dx**2 + dy**2)
 
     # Calculate magnitude of attractive field according to Lennard-Jones Potential
-    field_mag = -24*eps*(sigma**6)/(r_ij**7) # Includes negative to account for attraction
+    field_mag = -24e-9*eps*(sigma**6)/(r_ij**7) # Includes negative to account for attraction
 
     # Update electric field vector
     f_x = field_mag * dx/r_ij
@@ -31,7 +31,7 @@ def coulomb(l1, l2, charge1, charge2):
     dy = l1[1] - l2[1]
     r_ij = np.sqrt(dx**2 + dy**2)
 
-    f_mag = K_E*charge1*charge2 / (r_ij**2) # Includes sign to indicate attractive (-) or repulsive (+)
+    f_mag = K_E*(charge1*charge2 / (r_ij**2)) # Includes sign to indicate attractive (-) or repulsive (+)
 
     f_x = f_mag * (dx/r_ij)
     f_y = f_mag * (dy/r_ij)
@@ -45,7 +45,7 @@ def LJF_repulsive(l1, l2, eps, sigma):
     r_ij = np.sqrt(dx**2 + dy**2)
 
     # Calculate magnitude of repulsive field according to Lennard-Jones Potential
-    field_mag = 48*eps*(sigma**12)/(r_ij**13)
+    field_mag = 48e-9*eps*(sigma**12)/(r_ij**13)
 
     # Update electric field vector
     f_x = field_mag * dx/r_ij

main.py
@@ -9,6 +9,7 @@ from MD.types import ParticleType
 
 # GLOBALS
 e = 1.602*10**(-19)
+AVO = 6.022*10**(23)
 
 # NOTE: This is only for testing purposes
 def SHO_force(k, q):
@@ -24,40 +25,42 @@ def anifunc(n):
 '''
 
 def main():
-    lx = 10
+    lx = 100  # nm
+    x_range = np.array([0, lx])
 
     ### STEP 1: setup membrane ###
     material = Material() # NOTE see membrane.py for defaults
     thickness = 3
     x_centre = lx/2 # centre the membrane in the simulation area
-    height = 10
-    n_slices = 10
+    height = 100 # nm
+    y_range = np.array([0, height])
+    n_slices = 50
     membrane = Membrane(thickness, x_centre, height, material, n_slices)
 
     ### STEP 2: setup ions in solution ###
-    NC = 5  # number of charged catholyte ions
-    mass_c = 82.94 # mass of catholyte ions
+    NC = 10  # number of charged catholyte ions
+    mass_c = 82.94/AVO # mass of catholyte ions
     charge_c = e # charge of catholyte ions
-    NA = 5 # number of charged anolyte ions
-    mass_a = 50.94 # mass of anolyte ions
+    NA = 10 # number of charged anolyte ions
+    mass_a = 50.94/AVO # mass of anolyte ions
     charge_a = 2*e# charge of anolyte ions
     NP_C = 5 # number of protons cathode side
     NP_A = 5 # number of protons anode side
-    mass_p = 1 # mass of protons
+    mass_p = 1/AVO # mass of protons
     charge_p = e # charge of proton
     n_steps = 10**3
 
     # set up poisson object
-    poisson_pes = PoissonPES(nx=100, lx=1, p0=0, pl=1)
+    poisson_pes = PoissonPES(nx=100, lx=lx, p0=0, pl=1)
 
     # randomize initial particle positions and velocities
     # NOTE: putting catholyte on the left and anolyte on the right
-    particles = [Particle(mass_c, np.array([np.random.rand()*(x_centre-(thickness/2)), height*np.random.rand()]), 2*np.random.uniform(1,2)-1, charge_c, type=ParticleType.CATHODE_ION) for n in range(NC)]
-    particles += [Particle(mass_a, np.array([np.random.rand()*(x_centre-(thickness/2))+((x_centre+(thickness/2))), height*np.random.rand()]), 2*np.random.uniform(1,2)-1, charge_a, type=ParticleType.ANODE_ION) for n in range(NA)]
-    particles += [Particle(mass_p, np.array([np.random.rand()*(x_centre-(thickness/2)), height*np.random.rand()]), 2*np.random.uniform(1,2)-1, charge_p, type=ParticleType.PROTON) for n in range(NP_C)]
-    particles += [Particle(mass_p, np.array([np.random.rand()*(x_centre-(thickness/2))+((x_centre+(thickness/2))), height*np.random.rand()]), 2*np.random.uniform(1,2)-1, charge_p, type=ParticleType.PROTON) for n in range(NP_A)]
+    particles = [Particle(mass_c, np.array([np.random.rand()*(x_centre-(thickness/2)), height*np.random.rand()]), 2*np.random.uniform(1,2)-1, charge_c, x_range, y_range, type=ParticleType.CATHODE_ION) for n in range(NC)]
+    particles += [Particle(mass_a, np.array([np.random.rand()*(x_centre-(thickness/2))+((x_centre+(thickness/2))), height*np.random.rand()]), 2*np.random.uniform(1,2)-1, charge_a, x_range, y_range, type=ParticleType.ANODE_ION) for n in range(NA)]
+    particles += [Particle(mass_p, np.array([np.random.rand()*(x_centre-(thickness/2)), height*np.random.rand()]), 2*np.random.uniform(1,2)-1, charge_p, x_range, y_range, type=ParticleType.PROTON) for n in range(NP_C)]
+    particles += [Particle(mass_p, np.array([np.random.rand()*(x_centre-(thickness/2))+((x_centre+(thickness/2))), height*np.random.rand()]), 2*np.random.uniform(1,2)-1, charge_p, x_range, y_range, type=ParticleType.PROTON) for n in range(NP_A)]
     
-    verlet_integrator(particles, poisson_pes, membrane, n_steps, 0.1, system='NVT', gamma=10)
+    verlet_integrator(particles, poisson_pes, membrane, n_steps, 0.01, system='NVT', gamma=10)
 
     # NOTE: This is a WIP to do animation of particle positions
     '''